fa4a577d87f15b59afbcbf7755fd261774693fb3
dileep <dileepcp89@outlook.com>
2019-02-20 22:45:22 +0530

    Add project files.

---
+++ VedioRent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VedioRent.Startup))]
+namespace VedioRent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
